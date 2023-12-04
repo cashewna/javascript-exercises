a3632723fb0d421beff8e0cba92699d685f4639e
Krishna Khong <krishna@khong.dev>
2023-12-05 00:46:18 +0800

    feat: complete repeatString and its edge cases
    Repeat a given string n number of times and return "Error" when n
    is negative.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+/**
+ * Repeat the given string n times.
+ * @param {String} myString The string to repeat.
+ * @param {Integer} myNumber The number of times to repeat the string.
+ * returns {String} The repeated string.
+ */
+const repeatString = function(myString, myNumber) {
+    if (myNumber < 0) return 'ERROR';
+    return myString.repeat(myNumber); 
 };
 
 // Do not edit below this line
