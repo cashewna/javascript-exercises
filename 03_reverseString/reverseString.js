e57492034a996c6c55a8177bf91025fc4d1bfd02
Krishna Khong <krishna@khong.dev>
2023-12-05 01:32:49 +0800

    feat: complete reverseString and add docs

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+/**
+ * Reverses a given string.
+ * @param {String} toReverse The string to reverse.
+ * returns {String} The reversed string.
+ */
+const reverseString = function(toReverse) {
+    return toReverse.split("").reverse().join("");
 };
 
 // Do not edit below this line
