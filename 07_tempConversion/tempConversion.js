817204d07485eaa3465182ad75bb42c287df16f1
Krishna Khong <krishna@khong.dev>
2023-12-05 02:50:18 +0800

    feat: complete converToCelsius to spec

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,18 @@
-const convertToCelsius = function() {
+/**
+ * Converts a given temperature from Fahrenheit to Celsius
+ * @param {number} tempInFahrenheit 
+ * returns {number} tempInCelsius
+ */
+const convertToCelsius = function(tempInFahrenheit) {
+  if (typeof tempInFahrenheit !== 'number') {
+    return "ERROR";
+  }
+  
+  let tempInCelsius = (tempInFahrenheit - 32) * 5/9
+  if (tempInCelsius % 1 !== 0) {
+    tempInCelsius = Math.round(tempInCelsius * 10) / 10;
+  }
+  return tempInCelsius;
 };
 
 const convertToFahrenheit = function() {
