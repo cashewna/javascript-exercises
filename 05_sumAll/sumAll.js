e93ea8952e749e5a16d5063bb12b92332f5da4b0
Krishna Khong <krishna@khong.dev>
2023-12-05 02:24:51 +0800

    feat: complete sum all to spec

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    if (
+        arg1 < 0 || arg2 < 0 ||
+        typeof arg1 !== "number" || typeof arg2 !== "number"
+    ) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    let start = arg1 > arg2 ? arg2 : arg1;
+    let end = arg1 > arg2 ? arg1 : arg2;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
