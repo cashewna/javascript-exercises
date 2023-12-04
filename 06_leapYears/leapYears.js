bfa3c89c4f9a8c454d341c2c37a29b5959a5ab01
Krishna Khong <krishna@khong.dev>
2023-12-05 02:38:20 +0800

    feat: complete leap years to spec

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+/**
+ * Determines if a year is a leap year.
+ * @param {number} year 
+ * returns {boolean} true if year is a leap year, false otherwise
+ */
+const leapYears = function(year) {
+    if (year % 400 === 0) {
+        return true;
+    } else if (year % 100 === 0) {
+        return false;
+    } else if (year % 4 === 0) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
